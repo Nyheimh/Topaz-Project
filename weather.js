f8840f7eebf09a9b03679e5c113dacd180f3dac3
Nyheim Hunter <nyheimhunter@Nyheims-Air.attlocal.net>
2021-02-24 12:22:43 -0800

    second try catch

--- weather.js
+++ weather.js
@@ -7,13 +7,12 @@ let ASTRO_URL = `${DOMAIN}${ASTRO}?key=${API_KEY}&q=`;
 let FORECAST_URL = `${DOMAIN}${FORECAST}?key=${API_KEY}&q=`
 let DAY_URL = `${DOMAIN}${DAY}?key=${API_KEY}&q=`;
 
-async function weather(value) {
+const weather = async (value) => {
   const url = `${DAY_URL}${value}`
   // console.log(url)
     // console.log(sun)
 
   try {
-      removeWeather()
       const response = await axios.get(url)
       // console.log(response)
       let data = response.data
@@ -24,37 +23,40 @@ async function weather(value) {
       console.error(err)
     }
 }
-// weather()
-async function weather(value) {
-  const link = `${ASTRO_URL}${value}`
 
-  try {
-    removeWeather()
-  const response = await axios.get(link)
-  // console.log(response)
-  let dataStro = response.data.forecast.forecastday[0]
-    // weatherList(data)
-  console.log(dataStro)
-  return response
-} catch (err) {
-  console.error(err)
-}
-}
+// weather()
 
 // event listener
 const searchBtn = document.querySelector('#search')
 
 searchBtn.addEventListener('click', (e) => {
   e.preventDefault()
+  removeWeather()
 
   const searchBarValue = document.querySelector('#weather').value
 
   weather(searchBarValue)
 
   document.querySelector('#weather').value = ""
-
   
 })
+
+
+//new function for astro value
+async function astronomy(value) {
+  const link = `${ASTRO_URL}${value}`
+
+  try {
+    const response = await axios.get(link)
+  // console.log(response)
+  let dataStro = response.data.forecast.forecastday[0]
+    weatherList(dataStro)
+  // console.log(dataStro)
+  return response
+} catch (err) {
+  console.error(err)
+}
+}
 
 
 //  container
@@ -71,9 +73,13 @@ const weatherInfo = `
     <h3> Weather Condition: ${data.current.condition.text} </h3>
     <h3> ${data.current.condition.icon} </h3>
     
-
-    Sunset: ${data.forecast.forecastday[0].astro.sunset}
-
+  <div class="weatherAstro">
+  <h1> Astronomy </h1>
+    <p> Sunset: ${data.forecast.forecastday[0].astro.sunset} </p>
+    <p> Sunrise: ${data.forecast.forecastday[0].astro.sunrise} </p>
+    <p> Moon Phase: ${forecast.forecastday[0].astro.moon_phase} </p>
+    <p> Moonrise: ${data.forecast.forecastday[0].astro.moonrise} </p>
+    <p> Moonset: ${data.forecast.forecastday[0].astro.moonset} </p>
   </div>
 `
 weatherContainer.insertAdjacentHTML('beforeend', weatherInfo)
@@ -92,10 +98,4 @@ weatherContainer.insertAdjacentHTML('beforeend', weatherInfo)
 
 
 // </div>
-// <div class="weatherAstro">
-//   <h1> Astro </h1>
-//   <h2> Sunset: ${data.forecast.forecastday[0].astro.sunset} </h2>
-//   <h2> Sunset: ${data.forecast.forecastday[0].astro.sunset} </h2>
-//   <h2> Moon Phase: ${forecast.forecastday[0].astro.moon_phase} </h2>
-//   <h2> Moonrise: ${data.forecast.forecastday[0].astro.moonrise} </h2>
-//   <h2> Moonset: ${data.forecast.forecastday[0].astro.moonset} </h2>
+// 
