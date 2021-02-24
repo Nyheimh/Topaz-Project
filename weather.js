38915d53ed069952d1c4a3f25df22c47092687fc
Nyheim Hunter <nyheimhunter@Nyheims-Air.attlocal.net>
2021-02-24 13:38:20 -0800

    Almost there

--- weather.js
+++ weather.js
@@ -1,14 +1,14 @@
 const DOMAIN = 'http://api.weatherapi.com/v1/';
 let FORECAST = 'forecast.json'
-let ASTRO = 'astronomy.json'
-let DAY = 'current.json'
+// let ASTRO = 'astronomy.json'
+// let DAY = 'current.json'
 const API_KEY = '7d7cc083fad74e5193a213303212302'
-let ASTRO_URL = `${DOMAIN}${ASTRO}?key=${API_KEY}&q=`;
+// let ASTRO_URL = `${DOMAIN}${ASTRO}?key=${API_KEY}&q=`;
 let FORECAST_URL = `${DOMAIN}${FORECAST}?key=${API_KEY}&q=`
-let DAY_URL = `${DOMAIN}${DAY}?key=${API_KEY}&q=`;
+// let DAY_URL = `${DOMAIN}${DAY}?key=${API_KEY}&q=`;
 
 const weather = async (value) => {
-  const url = `${DAY_URL}${value}`
+  const url = `${FORECAST_URL}${value}`
   // console.log(url)
     // console.log(sun)
 
@@ -42,21 +42,21 @@ searchBtn.addEventListener('click', (e) => {
 })
 
 
-//new function for astro value
-async function astronomy(value) {
-  const link = `${ASTRO_URL}${value}`
+// new function for astro valuex
+// async function astronomy(info) {
+//   const link = `${ASTRO_URL}${info}`
 
-  try {
-    const response = await axios.get(link)
-  // console.log(response)
-  let dataStro = response.data.forecast.forecastday[0]
-    weatherList(dataStro)
-  // console.log(dataStro)
-  return response
-} catch (err) {
-  console.error(err)
-}
-}
+//   try {
+//     const response = await axios.get(link)
+//   // console.log(response)
+//   let dataStro = response.data.forecast.forecastday[0].astro
+//     weatherList(dataStro)
+//   // console.log(dataStro)
+//   return response
+// } catch (err) {
+//   console.error(err)
+// }
+// }
 
 
 //  container
@@ -65,37 +65,40 @@ function weatherList(data) {
 
   const weatherContainer = document.querySelector('.weather-info')
     // console.log(weatherContainer)
-
+// Add state to location in below div
 const weatherInfo = `
-  <div class ="weatherCurrent">
-    <h1> Location: ${data.location.name} </h1>
-    <h2> Current Temperature: ${data.current.temp_f} </h2>
-    <h3> Weather Condition: ${data.current.condition.text} </h3>
-    <h3> ${data.current.condition.icon} </h3>
-    
-  <div class="weatherAstro">
-  <h1> Astronomy </h1>
+  <div class ="weatherDate">
+    <p> Hello, the date is ${data.location.localtime}</p>
+
+    <div class ="weatherCurrent">
+    <p> Location: ${data.location.name}, ${data.location.region} </p>
+    <p> Current Temperature: ${data.current.temp_f} </p>
+    <p> Weather Condition: ${data.current.condition.text} </p>
+    </div>
+
+  <div class ="astronomy">
+    <h1>
     <p> Sunset: ${data.forecast.forecastday[0].astro.sunset} </p>
     <p> Sunrise: ${data.forecast.forecastday[0].astro.sunrise} </p>
-    <p> Moon Phase: ${forecast.forecastday[0].astro.moon_phase} </p>
+    <p> Moon Phase: ${data.forecast.forecastday[0].astro.moon_phase} </p>
     <p> Moonrise: ${data.forecast.forecastday[0].astro.moonrise} </p>
     <p> Moonset: ${data.forecast.forecastday[0].astro.moonset} </p>
-  </div>
+    </div>
+    
 `
-weatherContainer.insertAdjacentHTML('beforeend', weatherInfo)
+weatherContainer.insertAdjacentHTML('beforebegin', weatherInfo)
 }
 
 
   // remove previous data
   function removeWeather() {
-
     const weatherContainer = document.querySelector('.weather-info')
-
     while (weatherContainer.lastChild) {
       weatherContainer.removeChild(weatherContainer.lastChild)
     }
   }
 
-
-// </div>
-// 
+  // <div class="weatherAstro">
+  //   <h1> Astronomy </h1>
+  //     <p> Sunset: ${data.forecast.forecastday[0].astro.sunset} </p>
+      
