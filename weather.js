93a8f8c74805f5bfb29db33e1a452f2b211f025b
Nyheim Hunter <nyheimhunter@Nyheims-Air.attlocal.net>
2021-02-25 14:26:58 -0800

    clean up

--- weather.js
+++ weather.js
@@ -1,17 +1,16 @@
-const DOMAIN = 'http://api.weatherapi.com/v1/';
-let FORECAST = 'forecast.json'
+const domain = 'http://api.weatherapi.com/v1/';
+let forecast = 'forecast.json'
 // let ASTRO = 'astronomy.json'
 // let DAY = 'current.json'
-const API_KEY = '7d7cc083fad74e5193a213303212302'
+const api_key = '7d7cc083fad74e5193a213303212302'
 // let ASTRO_URL = `${DOMAIN}${ASTRO}?key=${API_KEY}&q=`;
-let FORECAST_URL = `${DOMAIN}${FORECAST}?key=${API_KEY}&days=3&q=`
+let forecast_url = `${domain}${forecast}?key=${api_key}&days=3&q=`
 // let DAY_URL = `${DOMAIN}${DAY}?key=${API_KEY}&q=`;
 
 const weather = async (value) => {
-  const url = `${FORECAST_URL}${value}`
+  const url = `${forecast_url}${value}`
   // console.log(url)
     // console.log(sun)
-
   try {
     removeWeather()
       const response = await axios.get(url)
@@ -24,59 +23,30 @@ const weather = async (value) => {
       console.error(err)
     }
 }
-
 // weather()
-
 // event listener
 const searchBtn = document.querySelector('#search')
-
-searchBtn.addEventListener('click', (e) => {
+  searchBtn.addEventListener('click', (e) => {
   e.preventDefault()
-
   const searchBarValue = document.querySelector('#weather').value
-
   weather(searchBarValue)
-
   document.querySelector('#weather').value = ""
-  
 })
 
-
-// new function for astro valuex
-// async function astronomy(info) {
-//   const link = `${ASTRO_URL}${info}`
-
-//   try {
-//     const response = await axios.get(link)
-//   // console.log(response)
-//   let dataStro = response.data.forecast.forecastday[0].astro
-//     weatherList(dataStro)
-//   // console.log(dataStro)
-//   return response
-// } catch (err) {
-//   console.error(err)
-// }
-// }
-
-
-//  container
-
 function weatherList(data) {
-
   const weatherContainer = document.querySelector('.weather-info')
-    // console.log(weatherContainer)
 // Add third day on line 82'
-  console.log(data.forecast)
-const weatherInfo = `
+  const weatherInfo = `
   <div class ="weatherDate">
     <p> Location: ${data.location.name}, ${data.location.region} </p>
   </div>
   <div class="box-container">
     <div class="current" id="box">  
-      <p> Current Temperature: ${data.current.temp_f} </p>
+      <p> Current Condition </p>
+      <p> Temperature: ${data.current.temp_f} </p>
       <p> Weather Condition: ${data.current.condition.text} </p>
     
-      <li> Day 1 Forecast: ${data.forecast.forecastday[1].day.avgtemp_f} </li>
+      <li> Day 1 Forecast: ${data.forecast.forecastday[0].day.avgtemp_f} </li>
       <li> Day 2 Forecast: ${data.forecast.forecastday[2].day.avgtemp_f}</li>
       <li> Day 3 Forecast: ${data.forecast.forecastday[2].day.avgtemp_f}</li>
     </div>
@@ -92,8 +62,6 @@ const weatherInfo = `
 `
 weatherContainer.insertAdjacentHTML('beforeend', weatherInfo)
 }
-
-
   // remove previous data
   function removeWeather() {
     const weatherContainer = document.querySelector('.weather-info')
